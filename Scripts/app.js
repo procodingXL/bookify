e80214701451d4680041709a4aca41bcaf49ea95
xigolle <joey.driessen@live.nl>
2016-03-21 10:03:27 +0100

    did shit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,18 @@
 ﻿var app = angular.module("bookify", []);
 
-bookify.controller("booklist",function($scope){
+app.controller("booklist", function ($scope) {
 
+});
+
+app.controller("itebooks", function ($scope,$http) {
+    console.log("inside itebooks controller");
+    $http({
+        method: 'GET',
+        url: 'http://it-ebooks-api.info/v1/'
+    }).then(function successCallback(response) {
+        //when successfull
+        console.log(response);
+    }, function errorCallback(response) {
+        //when failed
+    });
 });

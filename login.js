9dbd75fe8b88e7865ba8a159b91262952a1abf1e
mathiassamyn <mathiassamyn@hotmail.com>
2016-04-17 14:41:12 +0200

    omdat het moest

---
+++ login.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module("bookify");
+
+app.controller("login", function ($scope, $http) {
+    console.log("inside login controller");
+    test = function () {
+        alert('hjhhj');
+    }
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.5&appId=494568984079229";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    
+});

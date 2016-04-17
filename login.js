cd79a2a480c3518b05dbec55054d7f4e9a786068
mathiassamyn <mathiassamyn@hotmail.com>
2016-04-17 19:37:37 +0200

    kan info verkrijgen

--- login.js
+++ login.js
@@ -1,15 +1,61 @@
 ﻿var app = angular.module("bookify");
 
-app.controller("login", function ($scope, $http) {
-    console.log("inside login controller");
-    test = function () {
-        alert('hjhhj');
+//app.factory('facebookService', function ($q) {
+//    return {
+//        getMyLastName: function () {
+//            var deferred = $q.defer();
+//            FB.api('/me', {
+//                fields: 'last_name'
+//            }, function (response) {
+//                if (!response || response.error) {
+//                    deferred.reject('Error occured');
+//                } else {
+//                    deferred.resolve(response);
+//                }
+//            });
+//            return deferred.promise;
+//        }
+//    }
+//});
+
+app.controller("login", function ($scope, $http, $window) {
+   
+
+    //getMyLastName = function () {
+    //    facebookService.async.getMyLastName()
+    //      .then(function (response) {
+    //          $scope.last_name = response.last_name;
+    //          console.log($scope.last_name);
+    //      }
+    //    );
+    //};
+
+    getName = function() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response) {
+            console.log('Successful login for: ' + response.name);
+            $scope.name = response.name;
+            console.log(response);
+        });
     }
+    
+    $window.fbAsyncInit = function () {
+        FB.init({
+            appId: '494568984079229',
+            status: true,
+            cookie: true,
+            xfbml: true,
+            version: 'v2.5'
+        });
+    };
+    
+    //javascript
     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.5&appId=494568984079229";
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.6&appId=494568984079229";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 

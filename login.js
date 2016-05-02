69b419c3bc87b6e81965e1600fcd484fa6975217
mathiassamyn <mathiassamyn@hotmail.com>
2016-05-02 10:51:29 +0200

    magic happened

--- login.js
+++ login.js
@@ -18,7 +18,7 @@ app.controller("facebook", function ($scope, $window, facebookService) {
                 // request, and the time the access token 
                 // and signed request each expire
                 console.log("ingelogd");
-                getMyLastName();
+                getData();
                 var uid = response.authResponse.userID;
                 var accessToken = response.authResponse.accessToken;
             } else if (response.status === 'not_authorized') {
@@ -31,10 +31,12 @@ app.controller("facebook", function ($scope, $window, facebookService) {
     };
 
     getMyLastName = function () {
-        facebookService.getMyLastName()
+        facebookService.getData()
           .then(function (response) {
               $scope.last_name = response.last_name;
               console.log($scope.last_name);
+              console.log(response.first_name);
+              console.log(response.id);
           }
         );
     };
@@ -54,10 +56,10 @@ app.controller("facebook", function ($scope, $window, facebookService) {
 
 app.factory('facebookService', function ($q) {
     return {
-        getMyLastName: function () {
+        getData: function () {
             var deferred = $q.defer();
             FB.api('/me', {
-                fields: 'last_name'
+                fields: 'last_name, first_name'
             }, function (response) {
                 if (!response || response.error) {
                     deferred.reject('Error occured');

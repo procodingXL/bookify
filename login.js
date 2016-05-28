7bbc0c50301411af9350c4456e2441fcf19c058c
mathiassamyn <mathiassamyn@hotmail.com>
2016-05-28 21:22:51 +0200

    Stripe Working

--- login.js
+++ login.js
@@ -31,13 +31,17 @@ app.controller("facebook", function ($scope, $window, facebookService) {
         });
     };
 
+    
+
     getData = function () {
+        $scope.userName = "";
         facebookService.getData()
           .then(function (response) {
               $scope.last_name = response.last_name;
               console.log($scope.last_name);
               console.log(response.first_name);
               console.log(response.id);
+              $scope.userName = response.first_name;
           }
         );
     };
@@ -54,60 +58,7 @@ app.controller("facebook", function ($scope, $window, facebookService) {
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
-    /*
-    //Stripe
-    $scope.generateToken = function () {
-        var form = $('#payment-form');
-
-        // No pressing the buy now button more than once
-        form.find('button').prop('disabled', true);
-
-        // Create the token, based on the form object
-        Stripe.createToken(form, stripeResponseHandler);
-    };
-
-    var stripeResponseHandler = function (status, response) {
-        console.log("in responseHandler");
-        var form = $('#payment-form');
-
-        // Any validation errors?
-        if (response.error) {
-            // Show the user what they did wrong
-            form.find('.payment-errors').text(response.error.message);
-
-            // Make the submit clickable again
-            form.find('button').prop('disabled', false);
-        } else {
-            // Otherwise, we're good to go! Submit the form.
-
-            // Insert the unique token into the form
-            $('<input>', {
-                'type': 'hidden',
-                'name': 'stripeToken',
-                'value': response.id
-            }).appendTo(form);
-
-            console.log(response.id);
-            Stripe.setApiKey('sk_test_zJb3f0ZbLv5NULXBhqXhpzpH');
-            //Stripe.setSecretKey('sk_test_zJb3f0ZbLv5NULXBhqXhpzpH');
-
-            var charge = stripe.charges.create({
-                amount: 1000, // amount in cents, again
-                currency: "eur",
-                source: response.id,
-                description: "Example charge"
-            }, function (err, charge) {
-                if (err && err.type === 'StripeCardError') {
-                    // The card has been declined
-                }
-            });
-
-            // Call the native submit method on the form
-            // to keep the submission from being canceled
-            form.get(0).submit();
-          
-        }
-    };*/
+   
 })
 
 app.factory('facebookService', function ($q) {
@@ -129,40 +80,73 @@ app.factory('facebookService', function ($q) {
     }
 });
 
-//app.controller("login", function ($scope, $http, $window, facebookService) {
-//    console.log("in controller");
 
-//    getMyLastName = function () {
-//        facebookService.getMyLastName()
-//          .then(function (response) {
-//              $scope.last_name = response.last_name;
-//              console.log($scope.last_name);
-//          }
-//        );
-//    };
-    
-//    $window.fbAsyncInit = function () {
-//        FB.init({
-//            appId: '494568984079229',
-//            status: true,
-//            cookie: true,
-//            xfbml: true,
-//            version: 'v2.5'
-//        });
-//        FB.Event.subscribe('auth.login', function () {
-//            window.location = "http://localhost:5610/index.html";
-//        });
-//    };
-    
-//    //javascript
-//    (function(d, s, id) {
-//        var js, fjs = d.getElementsByTagName(s)[0];
-//        if (d.getElementById(id)) return;
-//        js = d.createElement(s);
-//        js.id = id;
-//        js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.6&appId=494568984079229";
-//        fjs.parentNode.insertBefore(js, fjs);
-//    }(document, 'script', 'facebook-jssdk'));
 
-    
-//});
+app.controller('paymentController', function ($scope, $http) {
+
+    //$scope.hideForm = false;
+
+    $scope.generateToken = function () {
+        var form = $('#payment-form');
+
+        // No pressing the buy now button more than once
+        //$scope.submitDis = true;
+        form.find('button').prop('disabled', true);
+
+        // Create the token, based on the form object
+        Stripe.createToken(form, stripeResponseHandler);
+
+    };
+
+    var stripeResponseHandler = function (status, response) {
+        console.log(response);
+        var form = $('#payment-form');
+
+        // Any validation errors?
+        if (response.error) {
+            // Show the user what they did wrong
+            //$scope.error = response.error.message;
+            form.find('.payment-errors').text(response.error.message);
+            // Make the submit clickable again
+            form.find('button').prop('disabled', false);
+            //$scope.submitDis = false;
+        } else {
+            // Otherwise, we're good to go! Submit the form.
+
+             //Insert the unique token into the form
+            $('<input>', {
+                'type': 'hidden',
+                'name': 'stripeToken',
+                'value': response.id
+            }).appendTo(form);
+          
+
+            console.log(response.id);
+
+            console.log(form.get(0));
+            //form.get(0).submit();
+            
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/api/posts',
+                data: $('#payment-form').serialize(),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).
+                then(function (response) {
+                    console.log(response.data);
+                    //if (response.data == "Transaction completed") {
+                    //    $scope.msgCompleted = response.data;
+                    //    $scope.hideForm = true;
+                    //} else {
+                    //    form.find('.payment-errors').text(response.data);
+                    //    form.find('button').prop('disabled', false);
+                        
+                    //} 
+                    $scope.msgCompleted = response.data;
+                    $scope.hideForm = true;
+                });
+
+        }
+    };
+})
+
---
+++ server.js
@@ -0,0 +1,40 @@
+﻿var express = require("express");
+var bodyparser = require("body-parser");
+var stripe = require("stripe")("sk_test_zJb3f0ZbLv5NULXBhqXhpzpH");
+
+
+var app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(3000);
+
+app.post("/api/posts", function (req, res) {
+    console.log('in post--server');
+    var stripeToken = req.body.stripeToken;
+    console.log(req.body);
+    console.log(stripeToken);
+    var charge = stripe.charges.create({
+        amount: 1000, // amount in cents
+        currency: "eur",
+        source: stripeToken,
+        description: "Example charge"
+    }, function (err, charge) {
+        if (err && err.type === 'StripeCardError') {
+            res.send("The card has been declined");
+        } else if (err) {
+            res.send("Something went wrong");
+        }
+        else res.send("Transaction completed");
+    });
+});
+
+
+
